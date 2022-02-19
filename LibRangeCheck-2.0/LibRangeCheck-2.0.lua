1fe6717a5333879c1fd64e7fb347e2c542836881
Simpy <kodi03466@hotmail.com>
2022-02-18 19:10:56 -0500

    adjustment from review

--- LibRangeCheck-2.0/LibRangeCheck-2.0.lua
+++ LibRangeCheck-2.0/LibRangeCheck-2.0.lua
@@ -50,17 +50,19 @@ local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
 local isClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
 local isTBC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
 
--- cache
--- GLOBALS: LibStub, CreateFrame
-local setmetatable = setmetatable
-local pairs = pairs
-local tostring = tostring
-local print = print
+-- GLOBALS: LibStub, CreateFrame, C_Map, FriendColor (??), HarmColor (??)
+local _G = _G
 local next = next
+local sort = sort
 local type = type
 local wipe = wipe
+local print = print
+local pairs = pairs
+local ipairs = ipairs
 local tinsert = tinsert
 local tremove = tremove
+local tostring = tostring
+local setmetatable = setmetatable
 local BOOKTYPE_SPELL = BOOKTYPE_SPELL
 local GetSpellInfo = GetSpellInfo
 local GetSpellBookItemName = GetSpellBookItemName
@@ -114,7 +116,7 @@ local InteractLists = {
 local MeleeRange = 2
 local FriendSpells, HarmSpells, ResSpells, PetSpells = {}, {}, {}, {}
 
-for _, n in next, { 'DEATHKNIGHT', 'DEMONHUNTER', 'DRUID', 'HUNTER', 'SHAMAN', 'MAGE', 'PALADIN', 'PRIEST', 'WARLOCK', 'WARRIOR', 'MONK', 'ROGUE' } do
+for _, n in ipairs({ 'DEATHKNIGHT', 'DEMONHUNTER', 'DRUID', 'HUNTER', 'SHAMAN', 'MAGE', 'PALADIN', 'PRIEST', 'WARLOCK', 'WARRIOR', 'MONK', 'ROGUE' }) do
     FriendSpells[n], HarmSpells[n], ResSpells[n], PetSpells[n] = {}, {}, {}, {}
 end
 
@@ -594,7 +596,8 @@ end
 -- minRange should be nil if there's no minRange, not 0
 local function addChecker(t, range, minRange, checker, info)
     local rc = { ["range"] = range, ["minRange"] = minRange, ["checker"] = checker, ["info"] = info }
-    for i, v in next, t do
+    for i = 1, #t do
+        local v = t[i]
         if rc.range == v.range then return end
         if rc.range > v.range then
             tinsert(t, i, rc)
@@ -608,7 +611,8 @@ local function createCheckerList(spellList, itemList, interactList)
     local res = {}
     if itemList then
         for range, items in pairs(itemList) do
-            for _, item in next, items do
+            for i = 1, #items do
+                local item = items[i]
                 if GetItemInfo(item) then
                     addChecker(res, range, nil, checkers_Item[item], "item:" .. item)
                     break
@@ -618,7 +622,8 @@ local function createCheckerList(spellList, itemList, interactList)
     end
 
     if spellList then
-        for _, sid in next, spellList do
+        for i = 1, #spellList do
+            local sid = spellList[i]
             local name, _, _, _, minRange, range = GetSpellInfo(sid)
             local spellIdx = findSpellIdx(name)
             if spellIdx and range then
@@ -680,8 +685,8 @@ local function updateCheckers(origList, newList)
         copyTable(newList, origList)
         return true
     end
-    for i, v in next, origList do
-        if v.range ~= newList[i].range or v.checker ~= newList[i].checker then
+    for i = 1, #origList do
+        if origList[i].range ~= newList[i].range or origList[i].checker ~= newList[i].checker then
             wipe(origList)
             copyTable(newList, origList)
             return true
@@ -703,7 +708,8 @@ end
 
 local function getMinChecker(checkerList, range)
     local checker, checkerRange
-    for _, rc in next, checkerList do
+    for i = 1, #checkerList do
+        local rc = checkerList[i]
         if rc.range < range then
             return checker, checkerRange
         end
@@ -713,7 +719,8 @@ local function getMinChecker(checkerList, range)
 end
 
 local function getMaxChecker(checkerList, range)
-    for _, rc in next, checkerList do
+    for i = 1, #checkerList do
+        local rc = checkerList[i]
         if rc.range <= range then
             return rc.checker, rc.range
         end
@@ -721,7 +728,8 @@ local function getMaxChecker(checkerList, range)
 end
 
 local function getChecker(checkerList, range)
-    for _, rc in next, checkerList do
+    for i = 1, #checkerList do
+        local rc = checkerList[i]
         if rc.range == range then
             return rc.checker
         end
@@ -830,8 +838,8 @@ function lib:init(forced)
     -- first try to find a nice item we can use for minRangeCheck
     local harmItems = HarmItems[15]
     if harmItems then
-        for _, item in next, harmItems do
-            local minCheck = minItemChecker(item)
+        for i = 1, #harmItems do
+            local minCheck = minItemChecker(harmItems[i])
             if minCheck then
                 minRangeCheck = minCheck
                 break
@@ -1170,7 +1178,7 @@ end
 local function pairsByKeys(t, f)
     local a = {}
     for n in pairs(t) do tinsert(a, n) end
-    table.sort(a, f)
+    sort(a, f)
     local i = 0
     local iter = function ()
         i = i + 1
